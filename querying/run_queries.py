cfefebcfecf47da2d82d312ebd5f279b554882e9
Daniel Bali <balijanosdaniel@gmail.com>
2012-07-30 01:34:51 +0200

    Added categories, mbid lookup, filtering

--- querying/run_queries.py
+++ querying/run_queries.py
@@ -6,10 +6,32 @@ import sys, utils, json, yaml, config
 from splunklib.client import connect
 from splunklib.binding import HTTPError
 
+TEST_MODE = True
+
+def look_up_mbid_list(mbid_list, mbid_dict, query_template, table_mapping, cursor):
+    for table, name_table in table_mapping.iteritems():
+        if mbid_list:
+            lookup_query = query_template % (table, name_table, "%s")
+            
+            try:
+                cursor.execute(lookup_query, (tuple(mbid_list),))
+            except Exception, e:
+                print "Query was: %s" % lookup_query
+                print e.pgerror
+                sys.exit(1)
+            
+            for record in cursor.fetchall():
+                # TODO: enable dict access, with string indices
+                mbid_dict[record[0]] = record[1]
+                mbid_list.remove(record[0])
+
 def main():
 
     # Parse the YAML file that contains queries
-    file = open('queries.yml')
+    if TEST_MODE:
+        file = open('queries_test.yml')
+    else:
+        file = open('queries.yml')
     queries = yaml.load(file.read())
     
     # Connect to splunk
@@ -22,50 +44,115 @@ def main():
     # Connect to the db
     try:
         db_conn = psycopg2.connect(config.DB_CREDENTIALS)
+        db_cursor = db_conn.cursor()
     except psycopg2.OperationalError, e:
         print e
         sys.exit(1)
-            
-    db_cur = db_conn.cursor()
     
-    # Run queries
-    for query_dict in queries:
-        # Taking the first element of the dictionary
-        # This is because of the YAML format
-        query = query_dict.values()[0]
+    # Loop through each category
+    for category_dict in queries['categories']:
+        category = category_dict.values()[0]
         
-        # Get response in JSON format
-        try:
-            response = splunk_conn.jobs.create(query['query'], exec_mode="oneshot", output_mode="json")
-        except HTTPError, e:
-            print "Splunk error: %s" % str(e.message)
-            sys.exit(1)
+        # Loop through each query in a category
+        for query_dict in category['queries']:
+            splunk_query = query_dict.values()[0]
             
-        print "Processing query: %s" % query['name']
-        data_json = json.loads(str(response))
-        data_json_wrapper = {'data': data_json, 'name': query['name']}
-        data = json.dumps(data_json_wrapper)
-        
-        try:
-            # Store results in db, commit
-            db_cur.execute("INSERT INTO log_statistic (report_type, data) VALUES (%s, %s);",
-                ("Test report type", str(data)))
-            db_conn.commit()
-        except Exception, e:
-            # Rollback
-            db_conn.rollback()
+            # Get Splunk query response
+            try:
+                response = splunk_conn.jobs.create(splunk_query['query'], exec_mode='oneshot', output_mode='json')
+            except HTTPError, e:
+                print "Splunk error: %s" % str(e.message)
+                sys.exit(1)
             
-            # Print error, return exit code 1
-            print e.pgerror
-            sys.exit(1)
+            # Load JSON    
+            print "Processing query: %s" % splunk_query['name']
+            data_json = json.loads(str(response))
             
+            # Try to find mbids in the response
+            mbid_list = []
+            for line in data_json:
+                if 'mbid' in line:
+                    mbid_list.append(line['mbid'])
+            
+            if mbid_list:
+                # Form query to find names for mbids
+                entity_query_template = """
+                    select x.gid, x_n.name 
+                    from %s x, %s x_n 
+                    where x.gid in %s 
+                    and x.name = x_n.id;
+                """
+                
+                redirect_query_template = """
+                    select x_r.gid, x_n.name 
+                    from %s x_r, %s x_n 
+                    where x_r.gid in %s
+                    and x_r.new_id = x_n.id
+                """
+                
+                # Mapping of entity tables, and name tables
+                entity_tables = {
+                    'artist': 'artist_name',
+                    'release_group': 'release_name',
+                    'release': 'release_name',
+                    'recording': 'track_name',
+                    'work': 'work_name',
+                    'label': 'label_name'
+                }
+                redirect_tables = {
+                    'artist_gid_redirect': 'artist_name',
+                    'release_group_gid_redirect': 'release_name',
+                    'release_gid_redirect': 'release_name',
+                    'recording_gid_redirect': 'track_name',
+                    'work_gid_redirect': 'work_name',
+                    'label_gid_redirect': 'label_name',
+                }
+                
+                # Store mbid - name pairs in a dict
+                mbid_dict = {}
+                look_up_mbid_list(mbid_list, mbid_dict, entity_query_template, entity_tables, db_cursor)
+                            
+                # Try to look up tables/ids with redirect tables   
+                # for mbids that weren't found in "normal" tables
+                look_up_mbid_list(mbid_list, mbid_dict, redirect_query_template, redirect_tables, db_cursor)
+                        
+            for line in data_json:
+                # Remove "private" key-value pairs
+                for key in line.keys():
+                    if key.startswith('_'):
+                        line.pop(key)
+                # Store names next to mbids in JSON
+                if 'mbid' in line:
+                    if line['mbid'] in mbid_dict:
+                        line['mbid_name'] = mbid_dict[line['mbid']]
+                    else:
+                        line['mbid_name'] = '{Name not found}'
+            
+            # Create a wrapper for JSON data
+            data_json_wrapper = {'data': data_json, 'name': splunk_query['name']}
+            data = json.dumps(data_json_wrapper)
+            
+            try:        
+                # Store results in db, commit
+                db_cursor.execute("INSERT INTO log_statistic (category, report_type, data) VALUES (%s, %s, %s);",
+                    (category['name'], 'TODO', str(data)))
+                db_conn.commit()
+            except Exception, e:
+                # Rollback
+                db_conn.rollback()
+                
+                # Print error, return exit code 1
+                print e.pgerror
+                sys.exit(1)
+                
     # Close connection
-    db_cur.close()
+    db_cursor.close()
     db_conn.close()
     
     # Exit code 0
     sys.exit(0)
+            
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
     
