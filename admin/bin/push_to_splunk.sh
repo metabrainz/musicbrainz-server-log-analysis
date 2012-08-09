352d53c63ab5866db0a64e24b616f6d6d416a325
Robert Kaye <rob@musicbrainz.org>
2012-08-09 23:46:56 +0000

    Remove comments. Makes it work better

--- admin/bin/push_to_splunk.sh
+++ admin/bin/push_to_splunk.sh
@@ -4,5 +4,4 @@ HOSTNAME=`hostname`
 SRC=/var/log/nginx/archive/musicbrainz-full-access.log-`date +%Y%m%d`
 DEST=logs@pino.mb:incoming/$HOSTNAME
 
-#rsync -z $SRC $DEST
-echo -z $SRC $DEST
+rsync -z $SRC $DEST
